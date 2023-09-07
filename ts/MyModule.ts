7d1cc378dae0d81c03b57897aef8da10d181a94c
Atano Okakura <atanookakura@gmail.com>
2023-09-07 11:31:45 +0900

    Updates

--- ts/MyModule.ts
+++
@@ -1,3 +0,0 @@
-export function myFunc(x: number, y: number): number {
-    return x + y;
-} 
--- tsup.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'tsup'
-
-export default defineConfig({
-  entry: ['ts/index.ts'],
-  splitting: false,
-  sourcemap: false,
-  clean: true,
-  outDir: 'js'
-})
